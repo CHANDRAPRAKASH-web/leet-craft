73959604ab43e233f7e09e6bf0b38c495871e179
CHANDRAPRAKASH <chandraprakash.manikandan@gmail.com>
2026-09-08 19:07:53 +0530

    Time: 196 ms (18.72%) | Memory: 20.6 MB (81.93%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -3,14 +3,14 @@ class Solution:
         l=1
         r=max(nums)
         res=r
-        while l<=r:
+        while l<r:
             mid=(l+r)//2
             time=0
             for i in range(len(nums)):
                 time+=math.ceil(nums[i]/mid)
             if time<=h:
                 res=min(res,mid)
-                r=mid-1
+                r=mid
             else:
                 l=mid+1
-        return res
+        return l
