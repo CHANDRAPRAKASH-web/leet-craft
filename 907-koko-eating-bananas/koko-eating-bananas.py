a18f607f9022212fe2742b880783065cbee1b8c9
CHANDRAPRAKASH <chandraprakash.manikandan@gmail.com>
2026-09-08 19:04:19 +0530

    Time: 211 ms (12.40%) | Memory: 20.8 MB (13.12%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minEatingSpeed(self, nums: List[int], h: int) -> int:
+        l=1
+        r=max(nums)
+        res=r
+        while l<=r:
+            mid=(l+r)//2
+            time=0
+            for i in range(len(nums)):
+                time+=((nums[i]-1)//mid)+1
+            if time<=h:
+                res=min(res,mid)
+                r=mid-1
+            else:
+                l=mid+1
+        return res
