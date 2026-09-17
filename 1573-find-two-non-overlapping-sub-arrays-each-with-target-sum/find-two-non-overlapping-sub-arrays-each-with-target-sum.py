61be1671e02933f5c39af68c22509ddd2196d15c
CHANDRAPRAKASH <chandraprakash.manikandan@gmail.com>
2026-09-17 11:38:49 +0530

    Time: 119 ms (68.16%) | Memory: 31.1 MB (66.48%) - LeetSync

--- 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.py
+++ 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.py
@@ -5,15 +5,14 @@ class Solution:
         dp=[inf]*n
         l=0
         p=0
-        for r in range(n):
+        for r in range(len(arr)):
             p+=arr[r]
             while p>target:
                 p-=arr[l]
                 l+=1
             dp[r]=dp[r-1] if r-1>=0 else inf
             if p==target:
-                res=min(res,r-l+1+dp[l-1] if l-1>=0 else inf)
-                dp[r]=min(dp[r-1],r-l+1)
+                res=min(res,(r-l+1)+dp[l-1] if l-1>=0 else inf)
+                dp[r]=min(r-l+1,dp[r-1])
         return -1 if res==inf else res
-
         
