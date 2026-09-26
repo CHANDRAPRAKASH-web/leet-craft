9199bc8d986930aca8e54e12b89bb6c9ff677b97
CHANDRAPRAKASH <chandraprakash.manikandan@gmail.com>
2026-09-26 11:31:07 +0530

    Time: 52 ms (52.34%) | Memory: 51.4 MB (61.68%) - LeetSync

--- 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.py
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.py
@@ -3,19 +3,19 @@ class Solution:
         a={}
         for i in knowledge:
             a[i[0]]=i[1]
-        started=False
+        start=False
         curr=""
         res=""
         for i in s:
             if i=='(':
-                started=True
+                start=True
             elif i==')':
                 v=a.get(curr,'?') 
                 res+=v
                 curr=''
-                started=False
-            elif started==False:
+                start=False
+            elif start==False:
                 res+=i
-            elif started==True:
+            elif start==True:
                 curr+=i
         return res       
